1485a9a0ec0e9225205e7475b9be46fa4a0465ad
danielcuneo <dpcuneo@fastmail.fm>
2015-09-13 13:10:50 -0700

    started over
    Former-commit-id: 79ef3ff590c5f762101363694eb5323b936bc4d4 [formerly bbbae3392470b27b93f66df356afdfc23858257e]
    Former-commit-id: 798e4547664be1dbd1b49136bea5f3e57cf0d939

--- DataScience/command_line_pres_data/sql_workbench_scratch.sql
+++ DataScience/command_line_pres_data/sql_workbench_scratch.sql
@@ -1,6 +1,21 @@
 use LinuxKernel;
+SELECT * FROM LinuxKernel.inventory;
 
-drop table Inventory;
+alter table inventory add category varchar(20);
 
-show tables;
-select * from inventory limit 10;
+select path from inventory where path in (select path_ from sub_list);
+
+update inventory set category = 
+       case when inventory.path in 
+	     (select path_ from sub_list)
+       then "linux" else NULL
+ end;
+
+-- join test
+select sub_list.path_
+from inventory 
+left join sub_list
+on inventory.path = sub_list.path_;
+
+
+select * from inventory limit 100;
