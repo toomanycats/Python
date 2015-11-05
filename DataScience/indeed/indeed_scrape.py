b969d000bbd004dd8933bc3ade0b0fc865613389
toomanycats <dpcuneo@fastmail.fm>
2015-11-05 11:19:05 -0800

    debugging
    Former-commit-id: d9a74573822682f5175c1f7b290d6f72dc66de83 [formerly 60a1f2b0d1558106b752459db5091250f054ed4f] [formerly d9a74573822682f5175c1f7b290d6f72dc66de83 [formerly 60a1f2b0d1558106b752459db5091250f054ed4f] [formerly 96b596486028d33ca1dec7e49c110f85be1429eb [formerly 2052099b1fa94b0e9573cc43d8878c9fdbc6a626]]]
    Former-commit-id: 96b596486028d33ca1dec7e49c110f85be1429eb
    Former-commit-id: 8e5baae2f35d3b9a211e8675f5b973bc87133082 [formerly b9579a31d3359fceb87d9447b06ed9d31209d5d6]
    Former-commit-id: 4e7db2e4d781bc4677580dc69252d79aeda0bde1

--- DataScience/indeed/indeed_scrape.py
+++ DataScience/indeed/indeed_scrape.py
@@ -3,7 +3,7 @@
 # Author : Daniel Cuneo
 # Creation Date : 11-05-2015
 ######################################
-#from indeed import IndeedClient
+import json
 import pandas as pd
 import urllib2
 from bs4 import BeautifulSoup
@@ -11,114 +11,122 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity, cosine_distances, euclidean_distances
 from sklearn.cluster import KMeans
 from sklearn import metrics
+import ipdb
 
-pub_id = "2818745629107228"
-api = 'http://api.indeed.com/ads/apisearch?publisher=%(pub_id)s&l=%(loc)s&q=%22data%20science%22&start=0&limit=25&st=employer&format=json&co=us&fromage=360&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2'
-
-#client = IndeedClient(pub_id)
-#params = {'q': "data science",
-#          'l': '',
-#          'format': 'json',
-#          'userip': '1.2.3.4',
-#          'useragent' :'Mozilla/%2F4.0%28Firefox%29',
-#          'v':'2',
-#          'start':'0',
-#          'limit':'25',
-#          'st':'employer'
-#         }
+class Indeed(object):
+    def __init__(self):
+#http://api.indeed.com/ads/apisearch?publisher=2818745629107228&q=%22data%20science%22&start=0&limit=4000&st=employer&format=json&co=us&fromage=360&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2
+        self.pub_id = "2818745629107228"
+        self.api = r'http://api.indeed.com/ads/apisearch?publisher=%(pub_id)s&l=%(loc)s&q=%%22data%%20science%%22&start=0&limit=25&st=employer&format=json&co=us&fromage=360&userip=1.2.3.4&useragent=Mozilla/%%2F4.0%%28Firefox%%29&v=2'
 
 
-def get_urls():
-    df = pd.read_csv('/home/daniel/git/Python2.7/DataScience/indeed/us_postal_codes.csv', dtype=str)
-    df.dropna(inplace=True)
+    def get_urls(self):
+        df= pd.read_csv('/home/daniel/git/Python2.7/DataScience/indeed/us_postal_codes.csv', dtype=str)
+        df.dropna(inplace=True)
 
-    locations = df['Postal Code'].dropna()
-    loc_samp = locations.sample(100)
+        locations = df['Postal Code'].dropna()
+        loc_samp = locations.sample(100)
 
-    urls = []
-    for loc in loc_samp:
-        #params['loc'] = loc
-        #data = client.search(**params)
-        api = api %{'pub_id':pub_id, 'loc':loc}
+        urls = []
+        for loc in loc_samp:
+            api = self.api %{'pub_id':self.pub_id,
+                            'loc':loc
+                            }
 
+            try:
+                response = urllib2.urlopen(api)
+                data = json.loads(response.read())
+                response.close()
+
+                try:
+                    url = [item['url'] for item in data['results']]
+
+                except Exception, err:
+                    print err
+                    continue
+
+            except urllib2.HTTPError, err:
+                print err
+                continue
+
+
+            urls.append(url)
+
+        return urls
+
+    def open_url(self, url):
         try:
-            response = urllib2.urlopen(api)
-            data = response.read()
+            response = urllib2.urlopen(url)
+            content = response.read()
+            return content
+
+        except urlib2.HTTPError:
+            return
+
+        finally:
             response.close()
 
-        except urllib2.HTTPError:
-            continue
+    def parse_content(self, content):
+        soup = BeautifulSoup(content, 'html.parser')
+        summary = soup.find('span', {'summary'})
 
-        urls.append(data['results']['url'])
+        return summary.get_text()
 
-    return urls
+    def main(self):
+        ipdb.set_trace()
+        df = pd.DataFrame()
+        df['url'] = self.get_urls()
+        df['summary'] = df['url'].apply(self.parse_content)
+        df = df.duplicated(keep='last')
 
-def open_url(url):
-    try:
-        response = urllib2.urlopen(url)
-        content = response.read()
-        return content
+        matrix, features = self.vectorizer(df['summary'])
+        print features
 
-    except urlib2.HTTPError:
-        return
+        df['assignments'] = self.cluster(matrix)
+        grp = df.groupby('assignments')
+        print grp
 
-    finally:
-        response.close()
+    def vectorizer(self, corpus):
+        vectorizer = TfidfVectorizer(max_features=30,
+                                    lowercase=True,
+                                    max_df=0.7,
+                                    min_df=0.1,
+                                    use_idf=True,
+                                    stop_words='english',
+                                    norm='l2',
+                                    ngram_range=(1, 1),
+                                    analyzer='word',
+                                    decode_error='ignore',
+                                    strip_accents='unicode'
+                                    )
 
-def parse_content(content):
-    soup = BeautifulSoup(content, 'html.parser')
-    summary = soup.find('span', {'summary'})
+        matrix = vectorizer.fit_transform(corpus)
+        features = vectorizer.get_feature_names()
 
-    return summary.get_text()
+        return matrix, features
 
-def main():
-    df = pd.DataFrame()
-    df['url'] = get_urls()
-    df['summary'] = df['url'].apply(parse_content)
+    def cluster(self, matrix):
 
-    matrix, features = vectorizer(df['summary'])
-    print features
+        cos_dist = cosine_distances(matrix)
+        euc_dist = euclidean_distances(matrix)
 
-    df['assignments']= cluster(matrix)
-    grp = df.groupby('assignments')
-    print grp
+        km = KMeans(k=3,
+                    init='random',
+                    #init='k-means++',
+                    n_init=100,
+                    max_iter=1000,
+                    tol=1e-7,
+                    precompute_distances=False,
+                    verbose=0,
+                    random_state=1,
+                    copy_x=True,
+                    n_jobs=4
+                    )
 
-def vectorizer(corpus):
-    vectorizer = TfidfVectorizer(max_features=30,
-                                lowercase=True,
-                                max_df=0.7,
-                                min_df=0.1,
-                                use_idf=True,
-                                stop_words='english',
-                                norm='l2',
-                                ngram_range=(1, 1),
-                                analyzer='word',
-                                decode_error='ignore',
-                                strip_accents='unicode'
-                                )
+        assignments = km.fit_predict(cos_dist)
 
-    matrix = vectorizer.fit_transform(corpus)
-    features = vectorizer.get_feature_names()
+        return assignments
 
-    return matrix, features
-
-def cluster(matrix):
-    cos_dist = cosine_distances(matrix)
-    euc_dist = euclidean_distances(matrix)
-
-    km = KMeans(k=3,
-                init='random',
-                #init='k-means++',
-                n_init=100,
-                max_iter=1000,
-                tol=1e-7,
-                precompute_distances=False,
-                verbose=0,
-                random_state=1,
-                copy_x=True,
-                n_jobs=4
-                )
-
-    assignments = km.fit_predict(cos_dist)
-
-    return assignments
+if __name__ == "__main__":
+    indeed = Indeed()
+    indeed.main()
