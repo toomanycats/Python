44c91c873273a7b0a1b446be5b67f150315dd8f0
toomanycats <dpcuneo@fastmail.fm>
2015-11-05 10:43:59 -0800

    ready to run and debug
    Former-commit-id: 67ce57b368f25a26232748b9e332b4f0abfe21a1 [formerly 340923784ff7ce5ecba8da1b85736cbebae2c03d]
    Former-commit-id: a40ffdc9af54354a89707cdcca03ab04c9940211

--- DataScience/indeed/indeed_scrape.py
+++ DataScience/indeed/indeed_scrape.py
@@ -3,37 +3,52 @@
 # Author : Daniel Cuneo
 # Creation Date : 11-05-2015
 ######################################
-from indeed import IndeedClient
+#from indeed import IndeedClient
 import pandas as pd
 import urllib2
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity, cosine_distances, euclidean_distances
+from sklearn.cluster import KMeans
+from sklearn import metrics
 
 pub_id = "2818745629107228"
+api = 'http://api.indeed.com/ads/apisearch?publisher=%(pub_id)s&l=%(loc)s&q=%22data%20science%22&start=0&limit=25&st=employer&format=json&co=us&fromage=360&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2'
 
-client = IndeedClient(pub_id)
-params = {'q': "data science",
-          'l': '',
-          'format': 'json',
-          'userip': '1.2.3.4',
-          'useragent' :'Mozilla/%2F4.0%28Firefox%29',
-          'v':'2',
-          'start':'0',
-          'limit':'25',
-          'st':'employer'
-         }
+#client = IndeedClient(pub_id)
+#params = {'q': "data science",
+#          'l': '',
+#          'format': 'json',
+#          'userip': '1.2.3.4',
+#          'useragent' :'Mozilla/%2F4.0%28Firefox%29',
+#          'v':'2',
+#          'start':'0',
+#          'limit':'25',
+#          'st':'employer'
+#         }
 
 
 def get_urls():
     df = pd.read_csv('/home/daniel/git/Python2.7/DataScience/indeed/us_postal_codes.csv', dtype=str)
+    df.dropna(inplace=True)
 
     locations = df['Postal Code'].dropna()
     loc_samp = locations.sample(100)
 
     urls = []
     for loc in loc_samp:
-        params['loc'] = loc
-        data = client.search(**params)
+        #params['loc'] = loc
+        #data = client.search(**params)
+        api = api %{'pub_id':pub_id, 'loc':loc}
+
+        try:
+            response = urllib2.urlopen(api)
+            data = response.read()
+            response.close()
+
+        except urllib2.HTTPError:
+            continue
+
         urls.append(data['results']['url'])
 
     return urls
@@ -62,6 +77,11 @@ def main():
     df['summary'] = df['url'].apply(parse_content)
 
     matrix, features = vectorizer(df['summary'])
+    print features
+
+    df['assignments']= cluster(matrix)
+    grp = df.groupby('assignments')
+    print grp
 
 def vectorizer(corpus):
     vectorizer = TfidfVectorizer(max_features=30,
@@ -81,3 +101,24 @@ def vectorizer(corpus):
     features = vectorizer.get_feature_names()
 
     return matrix, features
+
+def cluster(matrix):
+    cos_dist = cosine_distances(matrix)
+    euc_dist = euclidean_distances(matrix)
+
+    km = KMeans(k=3,
+                init='random',
+                #init='k-means++',
+                n_init=100,
+                max_iter=1000,
+                tol=1e-7,
+                precompute_distances=False,
+                verbose=0,
+                random_state=1,
+                copy_x=True,
+                n_jobs=4
+                )
+
+    assignments = km.fit_predict(cos_dist)
+
+    return assignments
