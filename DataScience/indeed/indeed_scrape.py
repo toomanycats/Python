269ad23e4271695cc00cca187745afae1e15d252
toomanycats <dpcuneo@fastmail.fm>
2015-11-05 10:17:15 -0800

    getting bs to done
    Former-commit-id: 60609e398e00fa409cb69cd203f8edc05fded073 [formerly 382034fc939482ca53ee2030e34379a4d9e889c7] [formerly 60609e398e00fa409cb69cd203f8edc05fded073 [formerly 382034fc939482ca53ee2030e34379a4d9e889c7] [formerly 3b6740be90afdba3385aa95195b6bcf3fc67eda5 [formerly fa46f49db05103898a0c109ae1273d0551984304]]]
    Former-commit-id: 3b6740be90afdba3385aa95195b6bcf3fc67eda5
    Former-commit-id: d05fedd3edd0cc06199ee5b6a5fdac45490ed32e [formerly 2e15c43b2534fcc5f2de4e50b189b4c34209fb96]
    Former-commit-id: 7b017c4563e3c8e46d41f4151e79e541ff2e1e72

--- DataScience/indeed/indeed_scrape.py
+++ DataScience/indeed/indeed_scrape.py
@@ -5,21 +5,23 @@
 ######################################
 from indeed import IndeedClient
 import pandas as pd
+import urllib2
+from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 pub_id = "2818745629107228"
 
 client = IndeedClient(pub_id)
 params = {'q': "data science",
-        'l': location,
-        'format': 'json',
-        'userip': '1.2.3.4',
-        'useragent' :'Mozilla/%2F4.0%28Firefox%29',
-        'v':'2',
-        'start':'0',
-        'limit':'25',
-        'st':'employer',
-        'fromage':age
-        }
+          'l': '',
+          'format': 'json',
+          'userip': '1.2.3.4',
+          'useragent' :'Mozilla/%2F4.0%28Firefox%29',
+          'v':'2',
+          'start':'0',
+          'limit':'25',
+          'st':'employer'
+         }
 
 
 def get_urls():
@@ -27,3 +29,55 @@ def get_urls():
 
     locations = df['Postal Code'].dropna()
     loc_samp = locations.sample(100)
+
+    urls = []
+    for loc in loc_samp:
+        params['loc'] = loc
+        data = client.search(**params)
+        urls.append(data['results']['url'])
+
+    return urls
+
+def open_url(url):
+    try:
+        response = urllib2.urlopen(url)
+        content = response.read()
+        return content
+
+    except urlib2.HTTPError:
+        return
+
+    finally:
+        response.close()
+
+def parse_content(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    summary = soup.find('span', {'summary'})
+
+    return summary.get_text()
+
+def main():
+    df = pd.DataFrame()
+    df['url'] = get_urls()
+    df['summary'] = df['url'].apply(parse_content)
+
+    matrix, features = vectorizer(df['summary'])
+
+def vectorizer(corpus):
+    vectorizer = TfidfVectorizer(max_features=30,
+                                lowercase=True,
+                                max_df=0.7,
+                                min_df=0.1,
+                                use_idf=True,
+                                stop_words='english',
+                                norm='l2',
+                                ngram_range=(1, 1),
+                                analyzer='word',
+                                decode_error='ignore',
+                                strip_accents='unicode'
+                                )
+
+    matrix = vectorizer.fit_transform(corpus)
+    features = vectorizer.get_feature_names()
+
+    return matrix, features
