8c8bc5e2bcf456597745e9eef4c38aa6a500c060
toomanycats <dpcuneo@fastmail.fm>
2015-11-07 15:33:43 -0800

    added mapper_init for indeed_scrape methods
    Former-commit-id: 2cdc378943e1ba09deb3df53ad0d16236803aed0 [formerly 8d80e43afe7550cc50dddd8cb4712c3d9ceaf8da] [formerly 2cdc378943e1ba09deb3df53ad0d16236803aed0 [formerly 8d80e43afe7550cc50dddd8cb4712c3d9ceaf8da] [formerly 1b36d104ce9c3efd4bde9768bf5372745b48f2a6 [formerly a42251e6ed3ac0f1b7b532390fe07dad4c2fbb7e]]]
    Former-commit-id: 1b36d104ce9c3efd4bde9768bf5372745b48f2a6
    Former-commit-id: 6a1912a6b766a22afc738455be3f4a710ab36693 [formerly 5205aefe7aac23aaee4e83a05e37f6909e4c4402]
    Former-commit-id: 9125ad38733a855a2e775e3c7d02cb74abf4e083

--- DataScience/indeed/map_red_scrape.py
+++ DataScience/indeed/map_red_scrape.py
@@ -7,21 +7,29 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
 import indeed_scrape
 
-ind = indeed_scrape.Indeed()
-ind.query = "data science"
-ind.build_api_string()
 
 class MRWorker(MRJob):
 
+    def configure_options(self):
+        super(MRWorker, self).configure_options()
+        self.add_file_option('--tokens')
+
+    def mapper_init(self):
+        self.ind = indeed_scrape.Indeed()
+        self.ind.query = "data science"
+        self.ind.config_path = "tokens.cfg"
+        self.ind.load_config()
+        self.ind.build_api_string()
+
     def mapper(self, _, line):
         zipcode, _,_,_,_,_,_= line.split(',')
-        urls = ind.get_url(zipcode)
+        urls = self.ind.get_url(zipcode)
         for url in urls:
             if url is not None:
                 yield url, None
 
     def reducer(self, url, _):
-        skills = ind.parse_content(url)
+        skills = self.ind.parse_content(url)
         if skills is not None:
             yield skills, None
 
