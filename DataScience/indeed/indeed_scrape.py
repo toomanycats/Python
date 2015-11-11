df23dc280de848484a126b2c576a1fe34b6be9e3
toomanycats <dpcuneo@fastmail.fm>
2015-11-11 15:07:07 -0800

    hack to avoid package bootstrapping in MRjob
    Former-commit-id: 6d8ecebb3b9ebe2e00963d9f709f484fb6c60c47 [formerly 0aad020804b124026be551c86d1dc0382fdb8432] [formerly 6d8ecebb3b9ebe2e00963d9f709f484fb6c60c47 [formerly 0aad020804b124026be551c86d1dc0382fdb8432] [formerly e04140b0217498ccf1dfa1f276032cc7c1727684 [formerly a06cfc06d017e3e310185f0106f13618cede6a65]]]
    Former-commit-id: e04140b0217498ccf1dfa1f276032cc7c1727684
    Former-commit-id: 050d3a2b8d15ae1ae17a8ecf5bc20219bdf9741a [formerly 831f5961105d7116c87f95e5f0e62455f98fc306]
    Former-commit-id: 66920edbaa9f916bd39fa3f8f3d9fb26271f485a

--- DataScience/indeed/indeed_scrape.py
+++ DataScience/indeed/indeed_scrape.py
@@ -14,14 +14,12 @@ from sklearn.feature_extraction.text import CountVectorizer, ENGLISH_STOP_WORDS
 from sklearn.metrics.pairwise import cosine_distances
 from sklearn.cluster import KMeans
 from sklearn.metrics import euclidean_distances
-from nltk import stem
-from nltk import tokenize
+#from nltk import stem
+#from nltk import tokenize
 import matplotlib.pyplot as plt
 import numpy as np
 import re
 
-toker = tokenize.word_tokenize
-stemmer = stem.SnowballStemmer('english')
 
 class Indeed(object):
     def __init__(self):
@@ -142,13 +140,22 @@ class Indeed(object):
 
         return new_list
 
+    def _load_toker(self):
+        # import nltk here, so that MRjob can run effiecently
+        from nltk import stem
+        from nltk import tokenize
+
+        self.toker = tokenize.word_tokenize
+        self.stemmer = stem.SnowballStemmer('english')
+
     def tokenizer(self, string):
+
         if string is None:
             return None
 
-        words = toker(string)
+        words = self.toker(string)
         words = self.len_tester(words)
-        words = map(stemmer.stem, words)
+        words = map(self.stemmer.stem, words)
 
         return " ".join(words)
 
@@ -229,6 +236,8 @@ class Indeed(object):
         save-on-quit feature more usable, the locations are shuffled prior to
         getting the content.'''
 
+        # annoying hack so that MRJob can use this module wo bootstrapping unecessary packages
+        self._load_toker()
         self.load_config()
         self.build_api_string()
         self.add_stop_words()
