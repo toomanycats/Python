5e01976232190c54c1a2f686d4daa3d46796862b
toomanycats <dpcuneo@fastmail.fm>
2015-11-12 09:53:04 -0800

    figured to use mapper's and no reducer
    Former-commit-id: e093727056dffb04934cefbf6a6444a569d7e0ab [formerly 95af21ff6a06d42d53abdd1977f0733c12433c3a]
    Former-commit-id: 29542517270921a1e5d6560e8f5e5753d75bf9dd

--- DataScience/indeed/map_red_scrape.py
+++ DataScience/indeed/map_red_scrape.py
@@ -1,12 +1,13 @@
-######################################
+#####################################
 # File Name : map_red_scape.py
 # Author : Daniel Cuneo
 # Creation Date : 11-07-2015
 ######################################
 from mrjob.job import MRJob
-#from mrjob.step import MRStep
+from mrjob.step import MRStep
 import indeed_scrape
 
+mrj = MRJob()
 
 class MRWorker(MRJob):
 
@@ -14,25 +15,37 @@ class MRWorker(MRJob):
         super(MRWorker, self).configure_options()
         self.add_file_option('--tokens')
 
-    def mapper_init(self):
+    def url_mapper_init(self):
         self.ind = indeed_scrape.Indeed()
         self.ind.query = "data science"
         self.ind.config_path = "tokens.cfg"
         self.ind.load_config()
         self.ind.build_api_string()
 
-    def mapper(self, _, line):
+    def url_mapper(self, _, line):
         zipcode, _,_,_,_,_,_= line.split(',')
         urls = self.ind.get_url(zipcode)
         for url, city in urls:
             yield city, url
 
-    def reducer_init(self):
+    def cont_mapper_init(self):
         self.ind = indeed_scrape.Indeed()
 
-    def reducer(self, url, city):
-        skills = self.ind.parse_content(url)
-        yield skills, city
+    def cont_mapper(self, city, url):
+        #mrj.set_status("reducer working")
+        content = self.ind.get_content(url)
+        yield city, content
+
+    def parse(self, city, content):
+        skills = self.ind.parse_content(content)
+        yield city, skills
+
+    def steps(self):
+        return [MRStep(mapper=self.url_mapper, mapper_init=self.url_mapper_init),
+                MRStep(mapper=self.cont_mapper, mapper_init=self.cont_mapper_init),
+                MRStep(mapper=self.parse, mapper_init=self.cont_mapper_init)
+               ]
+
 
 if __name__ == "__main__":
     MRWorker.run()
