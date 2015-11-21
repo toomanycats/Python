7aba962f8bf779bb70c3f74d4d9918fb5e6683b3
toomanycats <dpcuneo@fastmail.fm>
2015-11-21 13:02:14 -0800

    test 1 working
    Former-commit-id: dffea7a761139b5ec989c47391e6a3503a0b4be9 [formerly f1a74adc15aa153b19901e61b1b3d0fd2aacd6be]
    Former-commit-id: 0fd8351df0ecf716fed273602d83f1c179ad6994

--- DataScience/indeed/web_version.py
+++ DataScience/indeed/web_version.py
@@ -3,12 +3,14 @@
 # Author : Daniel Cuneo
 # Creation Date : 11-21-2015
 ######################################
+from flask import Flask
 import indeed_scrape
 import jinja2
 from bokeh.embed import components
 from bokeh.models.widgets import TextInput
 from bokeh.io import output_file, show, vform
 from bokeh.plotting import figure
+from bokeh.util.string import encode_utf8
 
 template = jinja2.Template("""
 <!DOCTYPE html>
@@ -37,16 +39,42 @@ template = jinja2.Template("""
 </html>
 """)
 
-output_file("text_input.html")
-text_input = TextInput(value="default", title="Search terms:")
-template.render(script=script, div=div)
-show(vform(text_input))
+app = Flask(__name__)
 
-ind = indeed_scrape.Indeed()
-ind.query = text_input
-ind.num_samp = 0
-ind.add_loc = '^(94)'
+def plot_fig():
+    p = figure(width=700, height=300)
+    p.title = "testing"
+    p.xaxis.axis_label = "key word"
+    p.yaxis.axis_label = "count"
+    p.line([1,2,3,4], [1,2,3,4])
 
-ind.main()
-df = ind.df
+    return p
 
+@app.route('/')
+def main():
+
+    p = plot_fig()
+    script, div = components(p)
+
+    #output_file("web_test.html")
+    #text_input = TextInput(value="default", title="Search terms:")
+    #show(vform(text_input))
+    html = template.render(script=script, div=div)
+
+    return encode_utf8(html)
+
+    #   ind = indeed_scrape.Indeed()
+    #   ind.query = text_input
+    #   ind.num_samp = 0
+    #   ind.add_loc = '^(94)'
+    #
+    #   # test
+    #   ind.handle_locations()
+    #   ind.locations = ind.locations[0:10]
+    #
+    #   ind.main()
+    #   df = ind.df
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(port=5050)
