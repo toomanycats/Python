55e272f3cd93ed8310795971485c5897b4f69da7
toomanycats <dpcuneo@fastmail.fm>
2015-12-13 14:15:54 -0800

    first commit
    Former-commit-id: baa5d912c6f9841034c5a60ff13d85a6ff32fe98 [formerly ad73ca8a43c48899d5096dad61ec2599755e0303]
    Former-commit-id: fa1dcc903830b6c37197f61cf6d04512c2f7aef2

---
+++ DataScience/GrammarParser.py
@@ -0,0 +1,88 @@
+''' https://gist.github.com/alexbowe/879414#file-nltk-intro-py-L34'''
+from nltk.corpus import stopwords
+
+stopwords = stopwords.words('english')
+lemmatizer = nltk.WordNetLemmatizer()
+stemmer_alt = nltk.stem.porter.PorterStemmer()
+
+# Used when tokenizing words
+sentence_re = r'''(?x)      # set flag to allow verbose regexps
+      ([A-Z])(\.[A-Z])+\.?  # abbreviations, e.g. U.S.A.
+    | \w+(-\w+)*            # words with optional internal hyphens
+    | \$?\d+(\.\d+)?%?      # currency and percentages, e.g. $12.40, 82%
+    | \.\.\.                # ellipsis
+    | [][.,;"'?():-_`]      # these are separate tokens
+'''
+# This grammar is from: S. N. Kim, T. Baldwin, and M.-Y. Kan.
+# Evaluating n-gram based evaluation metrics for automatic keyphrase extraction.
+# Technical report, University of Melbourne, Melbourne 2010.
+
+grammar = r"""
+    NBAR:
+        {<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
+
+    NP:
+        {<NBAR>}
+        {<NBAR><IN><NBAR>}  # Above, connected with in/of/etc...
+"""
+
+
+class Alternate(object):
+    """Fancier preprocessing of corpus using a grammar."""
+
+    def leaves(self, tree):
+        """Finds NP (nounphrase) leaf nodes of a chunk tree."""
+        for subtree in tree.subtrees(filter=lambda t:t.label() == 'NP'):
+            yield subtree.leaves()
+
+    def normalise(self, word):
+        """Normalises words to lowercase and stems and lemmatizes it."""
+        word = word.lower()
+        word = stemmer_alt.stem_word(word)
+        word = lemmatizer.lemmatize(word)
+
+        return word
+
+    def acceptable_word(self, word):
+        """Checks conditions for acceptable word: length, stopword."""
+        accepted = bool(2 <= len(word) <= 20
+            and word.lower() not in stopwords)
+
+        return accepted
+
+    def get_terms(self, tree):
+        """Filters the main tree and it's subtrees for 'leaves', normalizes the
+        words in the leaves and returns a generator."""
+
+        for leaf in self.leaves(tree):
+            term = [ self.normalise(w) for w,_ in leaf if self.acceptable_word(w) ]
+
+            yield term
+
+    def get_words(self, terms):
+        """Loops over the terms and returns a single string of the words."""
+
+        out = []
+        for term in terms:
+            for word in term:
+                out.append(word)
+
+        return " ".join(out)
+
+    def main(self, text):
+        """Breaks a single string into a tree using the grammar and returns
+        the specified words as a string."""
+
+        chunker = nltk.RegexpParser(grammar)
+
+        toks = nltk.regexp_tokenize(text, sentence_re)
+        postoks = nltk.tag.pos_tag(toks)
+
+        #print postoks
+        tree = chunker.parse(postoks)
+
+        terms = self.get_terms(tree)
+
+        words = self.get_words(terms)
+
+        return words
