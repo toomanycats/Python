ce953a56e44e55f3eb9a198e26d448e9710b3d70
toomanycats <dpcuneo@fastmail.fm>
2016-03-23 20:09:51 -0700

    sed after to clean up
    Former-commit-id: 9b9bc6278c414c0d505416338cc6301a467fdb53 [formerly 99dba67f6449adb8cc9a8fcfe9ab35c0cc6189d9] [formerly 9b9bc6278c414c0d505416338cc6301a467fdb53 [formerly 99dba67f6449adb8cc9a8fcfe9ab35c0cc6189d9] [formerly 5968ac7f6c3fe70b4c385d025f7c4e424b28538f [formerly 645b4f4d32544c8af33a9f9dbe387b79de611a1a]]]
    Former-commit-id: 5968ac7f6c3fe70b4c385d025f7c4e424b28538f
    Former-commit-id: 89626ebaf1fad2b217d8d1a39d04f737bbfd7d6d [formerly eeb83f02403b4e0add52abb2a8aabddbd9664a9e]
    Former-commit-id: 7333735dc9dd8ac54aaad86ce6c519c4090cb532

--- DataScience/notebooks/command_line_pres_data/map_reduce_ext_counter.py
+++ DataScience/notebooks/command_line_pres_data/map_reduce_ext_counter.py
@@ -1,17 +1,25 @@
-######################################
-# File Name : mr_chall.py
-# Author : Daniel Cuneo
-# Creation Date : 10-29-2015
-######################################
 from mrjob.job import MRJob
+from mrjob.step import MRStep
+from mrjob.util import bash_wrap
 
 class Process(MRJob):
 
-    def mapper_cmd(self):
-       return 'cut -d, -f 3', 1
+    def steps(self):
+        return [
+                MRStep(mapper_pre_filter = 'cut -d, -f 3',
+                       mapper = self.mapper,
+                       reducer = self.reducer
+                       ),
+                MRStep(mapper_cmd  = bash_wrap("sed 's/\"//g' | sed 's/\\\//g'")
+                       )
+                ]
+
+    def mapper(self, _, line):
+        yield line, 1
 
     def reducer(self, key, value):
         yield key, sum(value)
+
 
 if __name__ == "__main__":
     Process().run()
