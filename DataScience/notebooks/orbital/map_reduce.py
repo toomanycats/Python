48a2efd90455d1582aece2020f05da02790867e3
toomanycats <dpcuneo@fastmail.fm>
2016-03-25 17:41:27 -0700

    condensed into one liner
    Former-commit-id: 933b4b1c89c2840b8980ae4af56bff5a18949e93 [formerly d00a620f9a897d2e5942085807b7dfd0408cfbfa]
    Former-commit-id: 58c57ec9d2b7c01738b638305c21368146465ac0

--- DataScience/notebooks/orbital/map_reduce.py
+++ DataScience/notebooks/orbital/map_reduce.py
@@ -1,13 +1,11 @@
 from mrjob.job import MRJob
 from mrjob.util import bash_wrap
 
-class MRWordFrequencyCount(MRJob):
+class Grep(MRJob):
 
-    def mapper(self, _, line):
-        yield line.split(',')[0]
+    def mapper_cmd(self):
+      return bash_wrap("cut -d, -f 1 | pcregrep -no '(?sm)if\s*\(.*?\)' /dev/null | sed 's/\s//g'")
 
-    def reducer(self, _, line):
-        return bash_wrap("pcregrep -no '(?sm)if\s*\(.*?\)' /dev/null | sed 's/\s//g'")
 
 if __name__ == '__main__':
-    MRWordFrequencyCount.run()
+    Grep.run()
