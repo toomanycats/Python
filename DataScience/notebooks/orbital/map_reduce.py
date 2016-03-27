b978107d5ce743c2980852e546d3af8ecd983987
toomanycats <dpcuneo@fastmail.fm>
2016-03-27 09:40:49 -0700

    new working example all shell commands
    Former-commit-id: 9fce4fd8fbb719a066cb98168666a03e5220d9f3 [formerly 780b2e4358e29a0c394688b350925729125423f1] [formerly 9fce4fd8fbb719a066cb98168666a03e5220d9f3 [formerly 780b2e4358e29a0c394688b350925729125423f1] [formerly 340e5f7aaa6da6ea354882e4ba564523a1107307 [formerly a10ccd40bc08fff414a93d738186e3c56cabaa63]]]
    Former-commit-id: 340e5f7aaa6da6ea354882e4ba564523a1107307
    Former-commit-id: 39abb0866dba4b9334b1d3160b11a878fe8c4c4f [formerly c73000e37c58396810f4822433551ddc446cc30d]
    Former-commit-id: b014e67001282ddda2a504e311e91be912d27620

--- DataScience/notebooks/orbital/map_reduce.py
+++ DataScience/notebooks/orbital/map_reduce.py
@@ -1,11 +1,12 @@
 from mrjob.job import MRJob
-from mrjob.util import cmd_line, bash_wrap
-import os.path
+from mrjob.util import bash_wrap
 
-class Grep(MRJob):
-
+class ShellCmds(MRJob):
     def mapper_cmd(self):
-        return bash_wrap('shell_cmd.sh')
+        return bash_wrap('cut -d, -f 1 | xargs -n 1 grep -i "copyright" | sed "s/\W//g" | sed "s/[0-9]//g"')
+
+    def reducer_cmd(self):
+       return bash_wrap('sort | uniq -c | sort -n')
 
 if __name__ == '__main__':
-    Grep.run()
+    ShellCmds.run()
