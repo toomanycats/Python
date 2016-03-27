25567ee23f00543c08dfb6030a34c8903e387c18
toomanycats <dpcuneo@fastmail.fm>
2016-03-27 09:47:20 -0700

    working first draft
    Former-commit-id: 5c012860d68a90beb9f17ac3559575b386cc39d8 [formerly 9efd5d6b8850223a9db7869fc94d0776a5684b91]
    Former-commit-id: 8838b1ad43f4b108227924d8295b008c6842b2bc

--- DataScience/notebooks/orbital/shell_cmds.sh
+++ DataScience/notebooks/orbital/shell_cmds.sh
@@ -1,16 +1,5 @@
+#!/bin/bash
 ### script to parse out 'if' statements from C program files ###
 
-input=$1
+echo "$1" | cut -d, -f 1 | xargs -n 1  grep -i "copyright" | sed 's/\W//g' | sed 's/[0-9]//g'
 
-path=$(echo $input | cut -d, -f 1)
-echo $path
-
-#is_c_file=$(echo $path | grep -c -i '*.c')
-#
-#if [[ $is_c_file -gt 0 ]];then
-#    echo $(cat $path | pcregrep -no '(?sm)if\s*\(.*?\)' /dev/null | sed 's/\s//g')
-#else
-#    echo ""
-#fi
-
-exit 0
